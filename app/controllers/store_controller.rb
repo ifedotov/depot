e1a61edbbcf7b1bc315646584f2dcbe1c9d88f6f
ifedotov <ilya@wholelattelove.com>
2013-03-21 16:44:10 -0400

    Cart start

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
 	def index
+		if session[:counter].nil?
+			session[:counter] = 1
+		else 
+			session[:counter] += 1
+		end
+
+		@number_of_visits_message  = "You've been here #{session[:counter]} times" if session[:counter] >5
+
+		@visit_counter = session[:counter] 
 		@products = Product.order(:title)
 		@time = Time.now.strftime("%I:%M:%S")
 	end
