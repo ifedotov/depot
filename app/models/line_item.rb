b212266d0b57e8a3dfa60af381dcaca2a5f44980
Ilya Fedotov <ifedotov@gmail.com>
2013-03-21 23:48:47 -0400

    better cart before playtime

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,11 @@
 class LineItem < ActiveRecord::Base
-	attr_accessible :product, :product_id, :cart_id
+	attr_accessible :product, :product_id, :cart_id, :quantity
 
 	belongs_to :product
 	belongs_to :cart
+
+	def total_price
+		product.price * quantity
+	end
+
 end
